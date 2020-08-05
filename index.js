d6c04fcf1c1e504c56972fbc3df9e2bd5efeada3
yulia <juliana.ny2008@gmail.com>
2020-08-05 08:00:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+const  header = document.querySelector("#header");
+console.log(header)
 
 
 /***** Deliverable 2 *****/
 
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("PLAYERS array looks like this:", PLAYERS)
+
+
+
+let div = document.querySelector(".player-container")
+div.className = "player"
+
+for (let i=0; i < PLAYERS.length; i++ )
+
+{let h = document.createElement('h3')
+let img = document.createElement('img')
+div.appendChild(h)
+div.appendChild(img)
+h.innerHTML =  `${PLAYERS[i]["name"]} (<em>${PLAYERS[i]["nickname"]}</em>)`
+img.src = `${PLAYERS[i]["photo"]}`
+img.alt = `${PLAYERS[i]["name"]}` }
 
 
 /***** Deliverable 4 *****/
+let player = document.getElementsByTagName('h3') 
+for (let i=0; i < player.length; i++)
+{if (player[i].textContent = "Raheem Sterling") {player[i].remove()}}
+
+
